a08e36614224f4d4452c2dd8250db060d52f8330
aliintaa <146418081+aliintaa@users.noreply.github.com>
2024-09-14 09:54:43 +0300

    картинка работает

--- HandbookOfMedicines/MainWindow.xaml.cs
+++ HandbookOfMedicines/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HandbookOfMedicines.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace HandbookOfMedicines
         public MainWindow()
         {
             InitializeComponent();
+            frame.Navigate(new HumanPage());
         }
     }
 }
